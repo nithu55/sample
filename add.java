2de56cc8ff1fcc48930881d2e380b82e090360e2
unknown <nithudls99@gmail.com>
2020-02-24 11:46:12 +0530

    addition

---
+++ add.java
@@ -0,0 +1,20 @@
+class add
+{
+int a=10,b=20,c;
+void get();
+{
+c=a+b;
+}
+void print()
+{
+System.out.println(c);
+}
+class ad
+{
+public static void main(String args[])
+{
+add obj=new add():
+obj.get();
+obj.print();
+}
+}
